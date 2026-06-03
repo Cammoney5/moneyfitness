007510e5669765d58c14a4da16de0aa922a48562
Cammoney5 <cameronmoney5@hotmail.com>
2026-06-03 14:36:44 -0600

    Force service worker cache bust

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
+const CACHE_VERSION = 'v2';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE_VERSION; }).map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(
